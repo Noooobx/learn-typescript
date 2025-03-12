db67ceb5e5a9620b3ace9383799e8760f04caa49
SilentCodeSage <nandunandakishor345@gmail.com>
2025-03-12 18:56:12 +0530

    Set up TypeScript compilation with tsconfig.json and execution workflow

---
+++ dist/Tests/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let age = 21;
+if (age < 30)
+    console.log("Enjoyyy");
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ src/Tests/index.ts
@@ -0,0 +1,5 @@
+let age:number = 21;
+
+if(age < 30)
+    console.log("Enjoyyy");
+console.log(age);
